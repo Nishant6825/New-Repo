98c7bde2ddd779d0a15b02bf085ff6a63cfdea54
Nishant <nishant.joshi1109@gmail.com>
2022-12-26 16:45:32 +0530

    Moongoose Part 1

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes')
+
+const url = 'mongodb://127.0.0.1:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+    console.log("Connected to server");
+    var newDish = Dishes({
+        name: "Nishant",
+        description: 'Test'
+    });
+    newDish.save().then((dish)=>{
+        console.log(dish)
+        return Dishes.find({}).exec();
+
+    })
+    .then((dishes)=>{
+        console.log(dishes)
+        return Dishes.remove({})
+    }) 
+    .then(()=>{
+        return mongoose.connection.close();
+    }).catch((err)=>{
+        console.log(err)
+    })
+})
