2cfdaaae6e05eb7ba9207904a8f12bb093dc4118
Nishant <nishant.joshi1109@gmail.com>
2023-01-10 15:14:14 +0530

    Second Commit

---
+++ models/promotion.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+
+var promotionSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured:{
+        type: Boolean,
+        default: false,
+    }
+},{
+    timestamps: true
+})
+
+var Promotions = mongoose.model('Promotion', promotionSchema)
+module.exports = Promotions
