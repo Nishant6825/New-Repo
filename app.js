6d12aa30d7e55ba1a0cf9164ca219c1112303b62
Nishant <nishant.joshi1109@gmail.com>
2023-01-13 15:29:23 +0530

    JWT TOKEN

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ var session = require('express-session')
 var FileStore = require('session-file-store')(session);
 
 var passport = require('passport');
-var authenticate = require('./authenticate');
+
+var config = require('./config');
+
+
+
+const url = config.mongoUrl;
 
 var app = express();
 
@@ -22,9 +27,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 const mongoose = require('mongoose');
-const Dishes = require('./models/dishes')
 
-const url = 'mongodb://127.0.0.1:27017/conFusion';
+
+
 const connect =  mongoose.connect(url);
 
 connect.then((db)=>{
@@ -42,32 +47,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(passport.initialize());
-app.use(passport.session());
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-function auth (req, res, next) {
-  console.log(req.session);
-
-if(!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-}
-else {
-
-    next();
-  }
-}
-
-app.use(auth);
 
 
 app.use('/', indexRouter);
